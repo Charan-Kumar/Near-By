5f5cc519c58469b41feddcb34a3a86e1a02301fa
Charan Kumar Borra <charan@crypsis.net>
2015-04-15 18:50:41 +0530

    Working on IP

--- app/controllers/nearby_controller.rb
+++ app/controllers/nearby_controller.rb
@@ -7,7 +7,7 @@ class NearbyController < ApplicationController
  
   def hotel
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
-    @hotels = @client.spots(17.418481, 78.399456, :types => 'restaurant')
+    @hotels = @client.spots(lat, lng, :types => 'restaurant')
     @hash = Gmaps4rails.build_markers(@hotels) do |loc, marker|
       marker.lat loc.lat
       marker.lng loc.lng
@@ -16,7 +16,7 @@ class NearbyController < ApplicationController
 
   def shopping_malls
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
-    @sh = @client.spots(17.418481, 78.399456, :types => 'shopping_mall')
+    @sh = @client.spots(lat, lng, :types => 'shopping_mall')
     @hash = Gmaps4rails.build_markers(@sh) do |loc, marker|
       marker.lat loc.lat
       marker.lng loc.lng
@@ -25,7 +25,7 @@ class NearbyController < ApplicationController
 
   def parks
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
-    @parks = @client.spots(17.418481, 78.399456, :types => 'park')
+    @parks = @client.spots(lat, lng, :types => 'park')
     @hash = Gmaps4rails.build_markers(@parks) do |loc, marker|
       marker.lat loc.lat
       marker.lng loc.lng
@@ -34,7 +34,7 @@ class NearbyController < ApplicationController
 
   def movie_theaters
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
-    @mts = @client.spots(17.418481, 78.399456, :types => 'movie_theater')
+    @mts = @client.spots(lat, lng, :types => 'movie_theater')
 
     @hash = Gmaps4rails.build_markers(@mts) do |loc, marker|
       marker.lat loc.lat
@@ -45,7 +45,7 @@ class NearbyController < ApplicationController
 
   def hospitals
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
-    @hospitals = @client.spots(17.418481, 78.399456, :types => 'hospital')
+    @hospitals = @client.spots(lat, lng, :types => 'hospital')
     @hash = Gmaps4rails.build_markers(@hospitals) do |loc, marker|
       marker.lat loc.lat
       marker.lng loc.lng
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150414125951) do
-  
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -35,3 +35,4 @@ ActiveRecord::Schema.define(version: 20150414125951) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
+
