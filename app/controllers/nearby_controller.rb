15c1b29ca89a95e9c123c3a98e3008eb1bfa35c8
Charan Kumar Borra <charan@crypsis.net>
2015-04-16 12:35:26 +0530

    Working on IP

--- app/controllers/nearby_controller.rb
+++ app/controllers/nearby_controller.rb
@@ -2,7 +2,6 @@ class NearbyController < ApplicationController
 
   def hotel
     location = get_ip()
-    binding.pry
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
     @hotels = @client.spots(location.lat, location.lng, :types => 'restaurant')
     @hash = Gmaps4rails.build_markers(@hotels) do |loc, marker|
@@ -12,6 +11,7 @@ class NearbyController < ApplicationController
   end
 
   def shopping_malls
+    location = get_ip()
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
     @sh = @client.spots(location.lat, location.lng, :types => 'shopping_mall')
     @hash = Gmaps4rails.build_markers(@sh) do |loc, marker|
@@ -21,6 +21,7 @@ class NearbyController < ApplicationController
   end
 
   def parks
+    location = get_ip()
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
     @parks = @client.spots(location.lat, location.lng, :types => 'park')
     @hash = Gmaps4rails.build_markers(@parks) do |loc, marker|
@@ -30,6 +31,7 @@ class NearbyController < ApplicationController
   end
 
   def movie_theaters
+    location = get_ip()
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
     @mts = @client.spots(location.lat, location.lng, :types => 'movie_theater')
 
@@ -41,6 +43,7 @@ class NearbyController < ApplicationController
   end
 
   def hospitals
+    location = get_ip()
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
     @hospitals = @client.spots(location.lat, location.lng, :types => 'hospital')
     @hash = Gmaps4rails.build_markers(@hospitals) do |loc, marker|
