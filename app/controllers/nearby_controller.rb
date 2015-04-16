db71115cd5c369f5656a39fdf004f31c2e87572f
Charan Kumar Borra <charan@crypsis.net>
2015-04-16 11:34:31 +0530

    Working on IP

--- app/controllers/nearby_controller.rb
+++ app/controllers/nearby_controller.rb
@@ -1,54 +1,54 @@
 class NearbyController < ApplicationController
 
-  # ip = request.ip
-  # location =  Geokit::Geocoders::MultiGeocoder.geocode(ip)
-  # lat = location.lat
-  # lng = location.lng
+  @ip = request.ip
+  @location =  Geokit::Geocoders::MultiGeocoder.geocode(@ip)
+  @lat = @location.lat
+  @lng = @location.lng
  
   def hotel
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
-    @hotels = @client.spots(lat, lng, :types => 'restaurant')
+    @hotels = @client.spots(@lat, @lng, :types => 'restaurant')
     @hash = Gmaps4rails.build_markers(@hotels) do |loc, marker|
-      marker.lat loc.lat
-      marker.lng loc.lng
+      marker.@lat loc.@lat
+      marker.@lng loc.@lng
     end
   end
 
   def shopping_malls
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
-    @sh = @client.spots(lat, lng, :types => 'shopping_mall')
+    @sh = @client.spots(@lat, @lng, :types => 'shopping_mall')
     @hash = Gmaps4rails.build_markers(@sh) do |loc, marker|
-      marker.lat loc.lat
-      marker.lng loc.lng
+      marker.@lat loc.@lat
+      marker.@lng loc.@lng
     end
   end
 
   def parks
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
-    @parks = @client.spots(lat, lng, :types => 'park')
+    @parks = @client.spots(@lat, @lng, :types => 'park')
     @hash = Gmaps4rails.build_markers(@parks) do |loc, marker|
-      marker.lat loc.lat
-      marker.lng loc.lng
+      marker.@lat loc.@lat
+      marker.@lng loc.@lng
     end
   end
 
   def movie_theaters
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
-    @mts = @client.spots(lat, lng, :types => 'movie_theater')
+    @mts = @client.spots(@lat, @lng, :types => 'movie_theater')
 
     @hash = Gmaps4rails.build_markers(@mts) do |loc, marker|
-      marker.lat loc.lat
-      marker.lng loc.lng
+      marker.@lat loc.@lat
+      marker.@lng loc.@lng
     end
 
   end
 
   def hospitals
     @client = GooglePlaces::Client.new('AIzaSyB5inJaWD_E8Cn46Go123YzjQUPtZd7OJw')
-    @hospitals = @client.spots(lat, lng, :types => 'hospital')
+    @hospitals = @client.spots(@lat, @lng, :types => 'hospital')
     @hash = Gmaps4rails.build_markers(@hospitals) do |loc, marker|
-      marker.lat loc.lat
-      marker.lng loc.lng
+      marker.@lat loc.@lat
+      marker.@lng loc.@lng
     end
   end
 
